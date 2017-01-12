d293a5d742280d23231a2c6355ec945c7af9c286
Matthijs Neijenhuijs <matthijs@dutchbridge.nl>
2017-01-12 16:02:18 +0100

    first commmit

---
+++ packages/hideyo/shop/src/ShopServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hideyo\Shop;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShopServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        include __DIR__.'/Routes/frontend.php';
+        $this->app->make('Hideyo\Shop\Controllers\Frontend\BasicController');
+    }
+}
