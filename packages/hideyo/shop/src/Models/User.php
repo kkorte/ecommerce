6f5c96ce9647d17f0d44e41b55d5661c87ea225d
Matthijs Neijenhuijs <matthijs@dutchbridge.nl>
2017-01-23 11:47:54 +0100

    models

--- packages/hideyo/shop/src/Models/User.php
+++ packages/hideyo/shop/src/Models/User.php
@@ -49,7 +49,7 @@ class User extends Authenticatable
 
     public function getUserProfileData()
     {
-        return $this->hasMany('UserProfileData');
+        return $this->hasMany('Hideyo\Shop\Models\UserProfileData');
     }
 
     public function shop()
