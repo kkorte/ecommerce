ae40ad2c4e360323e0fa7a41e7013b1a06283d57
Matthijs Neijenhuijs <matthijs@dutchbridge.nl>
2017-12-05 19:52:53 +0100

    changes

--- resources/views/frontend/basic/index.blade.php
+++ resources/views/frontend/basic/index.blade.php
@@ -2,5 +2,36 @@
 
 @section('main')
 
+<div class="homepage-highlights">
+	<div class="row">
+		<div class="col-sm-3 col-md-3 col-lg-6">
+			<a href="/pants" title="pants">
+				<div class="highlight homepage-highlight-big">
+					<h3>Pants</h3>
+				</div>
+			</a>
+		</div>
+
+		<div class="col-sm-3 col-md-3 col-lg-3">
+			<a href="/dresses" title="dresses">
+				<div class="highlight homepage-highlight-small">
+					<h3>Dresses</h3>	
+				</div>
+			</a>
+		</div>
+
+		<div class="col-sm-3 col-md-3 col-lg-3">
+			<a href="/hats" title="dresses">
+				<div class="highlight homepage-highlight-small2">
+					<h3>Hats</h3>
+				</div>
+			</a>
+		</div>
+
+
+	</div>
+</div>
+
+
 
 @stop
